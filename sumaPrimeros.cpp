3833d7cdfbab3b357b7ec40b78c644d00946b191
jamamnica <jamamnica@unjbg.edu.pe>
2024-09-06 15:13:12 -0500

    Funcion SumaPrimerosNumeros

---
+++ sumaPrimeros.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int sumaPrimeros(int n){
+	int resp;
+	if(n==0){
+		
+		resp= 0;
+		
+	}else{
+		resp= n+sumaPrimeros(n-1);
+	}
+	return resp;
+}
+int main(){
+	int n;
+	cout<<"INTRODUZA LOS n PRIMEROS NUMEROS A SUMAR:"<<endl<<"n:  ";
+	cin>>n;
+	
+	if(n<0){
+		cout<<"intente nuevamente: ";
+		cin>>n;
+	}
+	
+	cout<<"Resultado: "<<sumaPrimeros(n);
+	return 0;
+}
+
